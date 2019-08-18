0e7f9ebd9eb10a73a939e7cca822b700131f9d20
Hasan Shodiev <uzbhasan@lwc.uz>
2019-08-18 15:10:10 +0500

    HomePageda til tanlash buttonida tanlangan davlat tilining bayrog'i turadigan qilindi

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { LanguagePopoverPage } from '../language-popover/language-popover.page';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,35 @@ import { LanguagePopoverPage } from '../language-popover/language-popover.page';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  languageFlag = '';
 
   constructor(
-    private popoverCtrl: PopoverController
+    private popoverCtrl: PopoverController,
+    private languageService: LanguageService
   ) {}
+
+  ngOnInit() {
+    this.setLanguageFlag();
+  }
+
+  setLanguageFlag() {
+    let languages = this.languageService.getLanguages();
+    let selected = this.languageService.selected;
+    for (let language of languages) {
+      if (language.value == selected) {
+        this.languageFlag = language.img;
+        break;
+      }
+    }
+  }
 
   async openLanguagePopover(ev) {
     const popover = await this.popoverCtrl.create({
       component: LanguagePopoverPage,
       event: ev
+    });
+    popover.onDidDismiss().then((data) => {
+      this.setLanguageFlag();
     });
     await popover.present();
   }
